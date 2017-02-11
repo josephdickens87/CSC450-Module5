d839e0ce431d51a61b640b47939305c78b34db5f
Joseph Dickens <josephdickens87@gmail.com>
2017-02-11 18:10:05 -0500

    pretty much done.

--- main.cpp
+++ main.cpp
@@ -1,60 +1,65 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
-
-
-
 
 
 using namespace std;
 
 int main()
 {
+    //declare variables.
     string name, age;
     fstream writer, inputFile, outputFile;
 
-
+    // get name.
     cout << "please tell me your name, then press enter." << endl;
-    getline (cin, name);
+    getline(cin, name);
 
-    cout << "please tell me your age, then press enter."<< endl;
-    getline (cin, age);
+    // get age.
+    cout << "please tell me your age, then press enter." << endl;
+    getline(cin, age);
 
+    // open text file.
     writer.open("mod5.txt", ios::app);
+
+    // throw error if can't open file.
     if (!writer)
     {
         cout << "Can't open file." << endl;
         exit(1);
     }
-    writer << name << age << endl;
+    // write to fle, then close it.
+    writer << name << " is " << age << " years old." << endl;
     writer.close();
 
 
+    // declare character vector, and stream object.
+    vector<char> charVec;
+    fstream file;
 
-
-    std::vector<char> charVec;
-    std::fstream file;
-
-    file.open("mod5.txt", std::ifstream::in | std::ifstream::binary);
-    file.seekg(0, std::ios::end);
-    std::streampos length(file.tellg());
+    //open file.
+    file.open("mod5.txt", ifstream::in | ifstream::binary);
+    //find end
+    file.seekg(0, ios::end);
+    streampos length(file.tellg());
+    // if statement to read get characters into vector.
     if (length)
     {
-        file.seekg(0, std::ios::beg);
-        charVec.resize(static_cast<std::size_t>(length));
-        file.read(&charVec.front(), static_cast<std::size_t>(length));
+        file.seekg(0, ios::beg);
+        charVec.resize(static_cast<size_t>(length));
+        file.read(&charVec.front(), static_cast<size_t>(length));
     }
 
-    for (int i = 0; i < charVec.size(); i++)
+    // loop through vector in reverse.
+    for (long i = charVec.size() - 1; i >= 0; i--)
     {
-        file.open("new.txt", std::ifstream::in | std:: ifstream:: app);
-        cout << charVec[i] << endl;
-        //file << charVec[i] << endl;
+        //open new text file.
+        file.open("new.txt", ifstream::in | ifstream::app);
+        // write the new file in reverse.
+        file << charVec[i];
+        //close file.
         file.close();
     }
-
-
 
     return 0;
 }
